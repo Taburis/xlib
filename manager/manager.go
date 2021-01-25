81460ef9c125514b5e506670db5994e775f484d5
TabrisXiao <tabris.xiao@gmail.com>
2021-01-25 13:01:41 -0600

    added md2html tool

---
+++ manager/manager.go
@@ -0,0 +1,119 @@
+package manager
+import (
+	//
+	
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"os"
+	//"errors"
+	"path/filepath"
+    "github.com/gomarkdown/markdown"
+    "github.com/gomarkdown/markdown/parser"
+    "github.com/gomarkdown/markdown/html"
+)
+
+type  Manager struct {
+	parser *parser.Parser
+	renderer *html.Renderer
+}
+
+func New() *Manager{
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	t := &Manager{parser: parser.NewWithExtensions(extensions),
+	 renderer: html.NewRenderer(opts),
+	}
+	return t
+}
+
+func (p *Manager) Config0() {
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	p.parser = parser.NewWithExtensions(extensions)
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	p.renderer = html.NewRenderer(opts)
+}
+
+
+func (p *Manager) MD2html(in_name string, out_name string) error{
+	// converting MD files to html format file
+	// in_name : abs path of the input md file
+	// out_name: abs path of the output html file
+	content , error := ioutil.ReadFile(in_name)
+	if error != nil {
+        return error
+    }	
+    p.Config0()
+    //fmt.Printf("here -------------------------------------------\n")
+    myhtml := markdown.ToHTML(content, p.parser, p.renderer)
+    //fmt.Printf("down -------------------------------------------\n")
+    error = ioutil.WriteFile(out_name, myhtml, 0644)
+    if error != nil{
+
+    	panic (error)
+    }
+
+    return error
+}
+
+func (p *Manager) ListDir(fileType string, path string) string{
+	// iterating over the folders (path) and return the list of 
+	// paths of files that satisfies the given type (fileType)
+	var list strings.Builder
+	filepath.Walk(path, func (path string, info os.FileInfo, err error) error{
+			if err != nil {
+				return err
+			}
+			s := strings.Split(path, ".")
+			if fileType == s[len(s)-1]{
+				list.WriteString(path)
+				list.WriteString("\n")
+				//fmt.Printf("found: %s\n",path)
+			}
+			return nil
+		})
+	return list.String()
+}
+
+func (p *Manager) DetachPath(in string) (path string, name string, ftype string){
+	// strip off the path and return the file name
+	fds := strings.Split(in, "/")
+	filename := fds[len(fds)-1]
+	filepart := strings.Split(filename, ".")
+	ftype = filepart[len(filepart)-1]
+	name = filepart[0]
+	if len(fds) > 1{
+		path = in[:len(in)-len(name)-len(ftype)-2]
+	} else {
+		path = ""
+	}
+	return 
+}
+
+func (p *Manager) Document(root string, dest string) error {
+	// root: root path of the inputs;
+	// dest: the root path of outputs.
+	if root[len(root)-1] == '/' {
+		root = root[:len(root)-1]
+	}
+	if dest[len(dest)-1] == '/' {
+		dest = dest[:len(dest)-1]
+	}
+	candidates := p.ListDir("md", root)
+	queue := strings.Split(candidates,"\n")
+	nroot := len(root)
+	for _,f := range queue{
+		if len(f) < nroot {continue}
+		ipath, ifile, _ := p.DetachPath(f[nroot+1:])
+		relpath := fmt.Sprintf("%s/%s",dest, ipath)
+		ofile   := fmt.Sprintf("%s/%s.%s", relpath, ifile, "html")
+		//fmt.Println(relpath)
+		fmt.Printf("Compiling: %s...\n",f)
+		os.MkdirAll(relpath, 0755)
+		p.MD2html(f, ofile)
+		fmt.Printf("finished, dump to %s\n", ofile)
+	}
+	return nil
+}
